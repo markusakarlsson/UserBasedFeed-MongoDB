738eaa7eae5c0c4b286e284fd04f5ac7d34d309c
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-05-07 17:44:22 +0200

    Basic input fields for new post

--- client/src/components/UserPage.jsx
+++ client/src/components/UserPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-
+import NewPost from "./NewPost";
 
 function UserPage() {
-    return(
-        <div style={{ border: "1px solid black", height: "100vh", width: "50vw"}}>
-            <h2>User page</h2>
-        </div>
-    );
-};
-
-
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        height: "100vh",
+        width: "50vw",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h2>User page</h2>
+      <NewPost></NewPost>
+    </div>
+  );
+}
 
 export default UserPage;
