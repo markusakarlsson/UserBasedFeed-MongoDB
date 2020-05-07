908ccec943c29a04c62ac092da47072fd657a377
Lisa <lisa.bengtsson@live.com>
2020-05-07 11:38:34 +0200

    GET all posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,16 +3,26 @@ const router = require("express").Router();
 let Post = require("../models/post.model");
 
 router.route("/add").post((req, res) => {
-    const username = req.body.username;
-    const title = req.body.title;
-    const textContent = req.body.textContent;
+  const username = req.body.username;
+  const title = req.body.title;
+  const textContent = req.body.textContent;
 
-    const newPost = new Post({ username, title, textContent });
+  const newPost = new Post({ username, title, textContent });
 
-    newPost
-        .save()
-        .then(() => res.json("Post posted!"))
-        .catch((err) => res.status(400).json("Error: " + err));
+  newPost
+    .save()
+    .then(() => res.json("Post posted!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
+
+router.route("/").get((req, res) => {
+  Post.find()
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+/* router.route("/update").put((req, res) => {
+ 
+}); */
 
 module.exports = router;
