c1b1116f842eae1b5b7ddb40a0ef2cdfcc4d83d2
Lisa <lisa.bengtsson@live.com>
2020-05-07 17:49:51 +0200

    trying to write json response on page

--- client/src/components/PostFeed.jsx
+++ client/src/components/PostFeed.jsx
@@ -1,14 +1,52 @@
 import React from "react";
+//import axios from "axios";
 
 
-function PostFeed() {
-    return (
-        <div className="postFeedContainer" style={{ border: "1px solid black", height: "100vh", width: "50vw"}}>
-          <h2>Post feed</h2>
-        </div>
-      );
-};
+class PostFeed extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      lists: []
+    }
+  }
 
 
+  getAllPosts() {
+    fetch('http://localhost:3000/posts/')
+    .then((response) => {
+      return response.json();
+    })
+    .then((posts) => {
+      console.log(posts);
+    });
+  };
+
+
+
+
+
+    render() {
+      return (
+          <div className="postFeedContainer" style={{ border: "1px solid black", height: "100vh", width: "50vw"}}>
+            <h2>Post feed</h2>
+            <div>{this.getAllPosts()}</div>
+           {/*  <div>
+            {this.state.lists.map((list) => {
+            <div key={list.username}>
+              <pre>
+                <h1>{JSON.stringify(list)}</h1>
+              </pre>
+            </div>
+        })}
+            </div> */}
+          </div>
+        );
+    }
+
+    
+
+
+}; 
 
 export default PostFeed;
