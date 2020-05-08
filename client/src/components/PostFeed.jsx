70d362e20b96c834eef3626611fe9b2b70269a62
Lisa <lisa.bengtsson@live.com>
2020-05-08 13:50:15 +0200

    displayed on page

--- client/src/components/PostFeed.jsx
+++ client/src/components/PostFeed.jsx
@@ -2,52 +2,52 @@ import React from "react";
 import axios from "axios";
 
 class PostFeed extends React.Component {
-
   state = {
-    posts: []
-  }
-
-  componentDidMount = () => {
-    this.getAllPosts()
-  }
-
-
-  getAllPosts = () => {
-    axios.get('http://localhost:3001/posts/')
-    .then((response) => {
-      this.setState({ posts: response.data });
-      console.log("In function this.state:", this.state)
-    })
-    .catch(() => {
-      alert("error retrieving data")
-    })
+    title: "",
+    textContent: "",
+    posts: [],
   };
 
+  componentDidMount = () => {
+    this.getAllPosts();
+  };
 
+  getAllPosts = () => {
+    axios
+      .get("http://localhost:3001/posts/")
+      .then((response) => {
+        this.setState({ posts: response.data });
+        console.log("In function this.state:", this.state);
+      })
+      .catch(() => {
+        alert("error retrieving data");
+      });
+  };
 
-
-
-    render() {
-      return (
-          <div className="postFeedContainer" style={{ border: "1px solid black", height: "100vh", width: "50vw"}}>
-            <h2>Post feed</h2>
-           {/*  <div>{this.getAllPosts()}</div> */}
-           {/*  <div>
-            {this.state.lists.map((list) => {
-            <div key={list.username}>
-              <pre>
-                <h1>{JSON.stringify(list)}</h1>
-              </pre>
-            </div>
-        })}
-            </div> */}
-          </div>
-        );
-    }
-
+  displayPosts = (posts) => {
     
+    if (!posts.length) return null;
+    
+    return posts.map((post, index) => (
+        <div key={index} style={{border: "1px solid black"}}>
+          <h3>Username: {post.username}</h3>
+          <h4>Title: {post.title}</h4>
+          <p>TextContent: {post.textContent}</p>
+        </div>
+    ));
+  };
 
-
-}; 
+  render() { 
+    return (
+      <div
+      className="postFeedContainer"
+        style={{ border: "1px solid black", height: "100vh", width: "50vw" }}
+      >
+        <h2>Post feed</h2>
+        {this.displayPosts(this.state.posts)}
+      </div>
+    );
+  }
+}
 
 export default PostFeed;
