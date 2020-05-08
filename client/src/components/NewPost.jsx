5cb5ca7b0ac2e656b992b4aae6ef8b8213b0f174
Lisa <lisa.bengtsson@live.com>
2020-05-08 15:18:00 +0200

    inputfields listening

--- client/src/components/NewPost.jsx
+++ client/src/components/NewPost.jsx
@@ -1,27 +1,55 @@
 import React from "react";
 
-function NewPost() {
-  return (
-    <div
-      style={{
-        border: "1px solid black",
-        height: "30vh",
-        width: "25vw",
-      }}
-    >
-      <h3>New post</h3>
-      <form>
-        <input type="text" placeholder="Title" />
-        <textarea
-          placeholder="Write your post here..."
-          id="newpost"
-          rows="4"
-          cols="50"
-        ></textarea>
-        <button type="submit">Send post</button>
-      </form>
-    </div>
-  );
+class NewPost extends React.Component {
+  state = {
+    title: "",
+    textContent: "",
+  };
+
+
+handleChange = (event) => {
+const target = event.target;
+const name = target.name;
+const value = target.value;
+
+this.setState({
+  [name]: value
+});
+};
+
+  render() {
+    console.log("State:", this.state)
+    return (
+      <div
+        style={{
+          border: "1px solid black",
+          height: "30vh",
+          width: "25vw",
+        }}
+      >
+        <h3>New post</h3>
+        <form>
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+            placeholder="Title"
+          />
+          <textarea
+            placeholder="Write your post here..."
+            name="textContent"
+            value={this.state.textContent}
+            onChange={this.handleChange}
+            id="newpost"
+            rows="4"
+            cols="50"
+          ></textarea>
+          <button type="submit">Send post</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewPost;
