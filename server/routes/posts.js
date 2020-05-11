d92bb285f56a820fab06e80b53201ce2e9a2dbd0
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-05-11 14:36:10 +0200

    Getting all post from one user

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,11 +2,11 @@
 const router = require("express").Router();
 let Post = require("../models/post.model");
 
-// router.route("/").get((req, res) => {
-//   Post.find()
-//     .then((posts) => res.json(posts))
-//     .catch((err) => res.status(400).json("Error: " + err));
-// });
+router.route("/").get((req, res) => {
+  Post.find()
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
 router.route("/getownposts").get((req, res) => {
   Post.find({ userId: req.session.userId }, function (err, posts) {
@@ -33,25 +33,25 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-// router.route("/:id").get((req, res) => {
-//   Post.findOne({ _id: req.params.id })
-//     .then((post) => res.json(post))
-//     .catch((err) => res.status(400).json("Error: " + err));
-// });
+router.route("/:id").get((req, res) => {
+  Post.findOne({ _id: req.params.id })
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-// router.route("/:id").put((req, res) => {
-//   Post.findOneAndUpdate(
-//     { _id: req.params.id },
-//     { title: "Updated title", textContent: "Updated text content" }
-//   )
-//     .then(() => res.json("Post updated!"))
-//     .catch((err) => res.status(400).json("Error: " + err));
-// });
+router.route("/:id").put((req, res) => {
+  Post.findOneAndUpdate(
+    { _id: req.params.id },
+    { title: req.body.title, textContent: req.body.textContent }
+  )
+    .then(() => res.json("Post updated!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-// router.route("/:id").delete((req, res) => {
-//   Post.findByIdAndDelete({ _id: req.params.id })
-//     .then(() => res.json("Post Deleted"))
-//     .catch((err) => res.status(400).json("Error: " + err));
-// });
+router.route("/:id").delete((req, res) => {
+  Post.findByIdAndDelete({ _id: req.params.id })
+    .then(() => res.json("Post Deleted"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
 module.exports = router;
