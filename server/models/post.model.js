a10b6adad2dc915c9bbc5e3e7a179583b0d3f8a1
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-05-11 14:10:02 +0200

    Work in progress

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   username: String,
   title: String,
   textContent: String,
