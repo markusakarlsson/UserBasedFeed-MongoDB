3debeb922e92756f0bf517edb10f8b4a80384bbe
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-05-12 17:41:14 +0200

    Unique username and removed __v

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String,
-});
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+  },
+  { versionKey: false }
+);
 
 const User = mongoose.model("User", userSchema);
 
